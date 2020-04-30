39426985c452906410aebc49ed788e1d40abcdb3
Steve Brownlee <chortlehoort@gmail.com>
2020-04-29 22:58:47 -0500

    Updated tests to not rely on headless browser

--- test/dom.test.js
+++ test/dom.test.js
@@ -1,33 +1,22 @@
+import { PotteryList } from "../src/scripts/PotteryList.js"
 import { makePottery } from "../src/scripts/PotteryWheel.js"
 import { firePottery } from "../src/scripts/Kiln.js"
 import { toSellOrNotToSell } from "../src/scripts/PotteryCatalog.js"
-const puppeteer = require("puppeteer")
 
-test('Pottery is rendered to DOM', async () => {
-    const mug = toSellOrNotToSell(firePottery(makePottery("Mug", 10, 8), 3000))
-    const ornament = toSellOrNotToSell(firePottery(makePottery("Ornament", 2, 5), 2000))
-    const platter = toSellOrNotToSell(firePottery(makePottery("Platter", 7, 2), 2000))
+describe('HTML representation', () => {
+    let html
 
-    let browser = await puppeteer.launch({
-        headless: true
+    beforeAll(() => {
+        const mug = toSellOrNotToSell(firePottery(makePottery("Mug", 10, 8), 3000))
+        const ornament = toSellOrNotToSell(firePottery(makePottery("Ornament", 2, 5), 2000))
+        const platter = toSellOrNotToSell(firePottery(makePottery("Platter", 7, 2), 2000))
+
+        html = PotteryList()
+        html = html.replace(/\n/g, "")
+        html = html.replace(/\s{2,}/g, "")
     })
-    const page = await browser.newPage();
-    await page.goto('http://localhost:8080');
-    await page.waitForSelector('.potteryList')
 
-    let section = await page.$eval('.potteryList', _ => _.innerHTML)
-
-    section = section.replace(/\n/g, "")
-    section = section.replace(/\s{2,}/g, "")
-
-    try {
-        expect(section).toBe(`<section class="pottery" id="pottery--2"><h2 class="pottery__shape">Ornament</h2><div class="pottery__properties">Item weighs 2 grams and is 5 cm in height</div><div class="pottery__price">Price is $20</div></section><section class="pottery" id="pottery--3"><h2 class="pottery__shape">Platter</h2><div class="pottery__properties">Item weighs 7 grams and is 2 cm in height</div><div class="pottery__price">Price is $40</div></section>`);
-    }
-    catch (error) {
-        throw error
-    }
-    finally {
-        browser.close()
-    }
-
+    test('Pottery are converted to HTML correctly', () => {
+        expect(html).toBe("<section class=\"pottery\" id=\"pottery--2\"><h2 class=\"pottery__shape\">Ornament</h2><div class=\"pottery__properties\">Item weighs 2 grams and is 5 cm in height</div><div class=\"pottery__price\">Price is $20</div></section><section class=\"pottery\" id=\"pottery--3\"><h2 class=\"pottery__shape\">Platter</h2><div class=\"pottery__properties\">Item weighs 7 grams and is 2 cm in height</div><div class=\"pottery__price\">Price is $40</div></section>");
+    })
 })
--- test/pottery.test.js
+++ test/pottery.test.js
@@ -2,16 +2,12 @@ import { makePottery } from "../src/scripts/PotteryWheel.js"
 import { firePottery } from "../src/scripts/Kiln.js"
 import { usePottery, toSellOrNotToSell } from "../src/scripts/PotteryCatalog.js"
 
-let piece = null
 
-beforeAll(() => {
-    piece = makePottery("Mug", 10, 8)
-})
+let piece = null
+piece = makePottery("Mug", 10, 8)
 
 test('Pottery object is created with correct properties', () => {
-    expect(
-        piece
-    )
+    expect(piece)
         .toMatchObject(
             { shape: "Mug", weight: 10, height: 8, id: 1 }
         )
@@ -23,11 +19,11 @@ describe('Firing the pottery in the kiln that is too hot', () => {
     })
 
     test('Pottery object is cracked when temperature is above 2200', () => {
-        expect( piece.cracked ).toBe( true )
+        expect(piece.cracked).toBe(true)
     })
 
     test('Pottery marked as fired after going in the kiln', () => {
-        expect( piece.fired ).toBe( true )
+        expect(piece.fired).toBe(true)
     })
 })
 
@@ -38,29 +34,11 @@ describe('Firing the pottery in the kiln with the correct temperate', () => {
     })
 
     test('Pottery object is uncracked when temperature is below 2200', () => {
-        expect( piece.cracked ).toBe( false )
+        expect(piece.cracked).toBe(false)
     })
 
     test('Pottery marked as fired after going in the kiln', () => {
-        expect( piece.fired ).toBe( true )
-    })
-})
-
-describe('Do not sell cracked pottery', () => {
-    let catalog = []
-
-    beforeAll(() => {
-        piece = firePottery(piece, 2300)
-        toSellOrNotToSell(piece)
-        catalog = usePottery()
-    })
-
-    test('Piece is not priced when cracked', () => {
-        expect( "price" in piece ).toBe( false )
-    })
-
-    test('Not in array of items to sell when cracked', () => {
-        expect( catalog.length ).toBe( 0 )
+        expect(piece.fired).toBe(true)
     })
 })
 
@@ -74,21 +52,35 @@ describe('Sell uncracked pottery', () => {
     })
 
     test('Piece is priced when not cracked', () => {
-        expect( "price" in piece ).toBe( true )
-        expect( piece.price ).toBe( 40 )
+        expect("price" in piece).toBe(true)
+        expect(piece.price).toBe(40)
     })
 
     test('In array of items to sell when uncracked', () => {
-        expect( catalog.length ).toBe( 1 )
+        expect(catalog.length).toBe(1)
     })
 
     test('Piece has correct price', () => {
-        expect( piece.price ).toBe( 40.00 )
+        expect(piece.price).toBe(40.00)
     })
 })
 
+describe('Do not sell cracked pottery', () => {
+    let catalog = []
+    let vase = makePottery("Vase", 4, 9)
 
+    beforeEach(() => {
+        vase = firePottery(vase, 3000)
+        toSellOrNotToSell(vase)
+        catalog = usePottery()
+    })
 
+    test('Piece is not priced when cracked', () => {
+        expect("price" in vase).toBe(false)
+    })
 
-
+    test('Not in array of items to sell when cracked', () => {
+        expect(catalog.find(p => p.shape === "Vase")).toBeUndefined()
+    })
+})
 
